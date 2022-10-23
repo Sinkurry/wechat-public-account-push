9a5cf382e05c0b4a42734eaf78a30eb22f0ce345
Sinkurry <116481127+Sinkurry@users.noreply.github.com>
2022-10-23 15:24:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -56,7 +56,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o8RhY6xaIy_MXsYgSBer3jEOJ5Us',
+      id: 'wxf3e384b0c9ae4713wxf3e384b0c9ae4713',
     }
   ],
 
