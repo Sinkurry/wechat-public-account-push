e219a1ded49ca32b27e0e94279b2a93e6bbdaea5
Sinkurry <116481127+Sinkurry@users.noreply.github.com>
2022-10-23 15:47:09 +0800

    Update and rename config/index.cjs to 配置/index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -19,31 +19,32 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝',
+      name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o8RhY6xaIy_MXsYgSBer3jEOJ5Us',
+      id: 'gh_4c77a1870b54',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	m1I8ZqpHIDGN7RgWpRX_x7EoT-d21pwm0CMsRR9eF2c',
+      useTemplateId: 'm1I8ZqpHIDGN7RgWpRX_x7EoT-d21pwm0CMsRR9eF2c',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-15',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '生日', name: '子逸', year: '1999', date: '01-19',
+          type: '*生日', name: '宝贝', year: '1998', date: '08-15',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '伟力', year: '1998', date: '08-15',
+          type: '生日', name: '宝贝', year: '1998', date: '08-15',
         },
         {
-          type: '节日', name: '相爱纪念日', year: '2022', date: '09-29',
+          type: '节日', name: '恋爱纪念日', year: '2022', date: '09-29',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2022-09-29' },
- 
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
@@ -63,4 +64,3 @@ const USER_CONFIG = {
 }
 
 module.exports = USER_CONFIG
-
